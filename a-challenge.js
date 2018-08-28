a190f4c91a87568cd994d0fff7807edbba5b57ba
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2018-08-28 18:20:28 +0200

    Create a-challenge.js

---
+++ a-challenge.js
@@ -0,0 +1,43 @@
+// for tddbin.com
+
+describe('mocha+assert API description', function() {
+  
+  it('can add values to the end', function() {
+    obj.push('a');
+    assert.deepEqual(obj[0], 'a');
+    obj.push('b');
+    assert.deepEqual(obj[1], 'b');
+  });
+
+  it('can remove values from the end', function() {
+    obj.pop();
+    assert.deepEqual(obj[1], undefined);
+    assert.deepEqual(obj[0], 'a');
+    obj.pop();
+    assert.deepEqual(obj[0], undefined);
+  });
+
+  it('can read arbitrary values', function() {
+    for (let i = 0; i < 5; i++) {
+      obj.push(i);
+    };
+    assert.deepEqual(obj[4], 4);
+    assert.deepEqual(obj[3], 3);
+    assert.deepEqual(obj[2], 2);
+    assert.deepEqual(obj[1], 1);
+    assert.deepEqual(obj[0], 0);
+  });
+
+  it('can remove arbitrary elements', function() {
+    obj.pop(2);
+    assert.deepEqual(obj[2], 3);
+  });
+
+  it('throws error if un-poppable is popped', function() {
+    for (let i = 0; i < 4; i++) {
+      obj.pop();
+    };
+    assert.throws(obj.pop());
+  });
+  
+});
