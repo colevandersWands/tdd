0fd6f3392164f93068207ab422717de137711af0
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2019-10-14 22:52:06 +0200

    pushable

--- exercises/loader.js
+++ exercises/loader.js
@@ -6,15 +6,16 @@ const exercises = {
   ],
   beginner: [
     'evedont',
-    'kishy'
+    'kishy',
+    { e: ['e', 'e '] }
   ],
   ['actual-beginner']: [
     'poo'
   ]
 }
 
-const loader = (exercise) => {
-  fetch(`${href}/exercises/${exercise}.js`)
+const loader = (exercisePath) => {
+  fetch(`${href}/${exercisePath}.js`)
     .then(function (response) {
       return response.text();
     })
@@ -27,17 +28,41 @@ const loader = (exercise) => {
     })
 }
 
-const exerciseDetails = document.getElementById('exercise-details');
-for (let collectionKey in exercises) {
-  const collectionDetails = document.createElement('details');
-  const collectionSummary = document.createElement('summary');
-  collectionSummary.innerHTML = collectionKey;
-  collectionDetails.appendChild(collectionSummary);
-  exercises[collectionKey].forEach(exerciseName => {
+
+{
+  const renderItem = (item, path, container) => {
+    if (item instanceof Array) {
+      return renderArray(item, path, container);
+    } else if (typeof item === 'string') {
+      return renderString(item, path, container);
+    } else if (item && item.constructor.name === 'Object') {
+      return renderObject(item, path, container);
+    }
+  };
+  const renderObject = (obj, path, container) => {
+    for (let key in obj) {
+      const details = document.createElement('details');
+      details.style = 'margin-left:15px';
+      const summary = document.createElement('summary');
+      summary.innerHTML = key;
+      details.appendChild(summary);
+      renderItem(obj[key], `${path}/${key}`, details);
+      container.appendChild(details);
+    };
+  };
+  const renderArray = (arr, path, container) => {
+    arr.forEach(entry => {
+      renderItem(entry, path, container);
+    });
+  };
+  const renderString = (name, path, container) => {
     const button = document.createElement('button');
-    button.innerHTML = exerciseName.split('-').join(' ');
-    button.onclick = () => { loader(`${collectionKey}/${exerciseName}`) };
-    collectionDetails.appendChild(button);
-  });
-  exerciseDetails.appendChild(collectionDetails);
+    button.innerHTML = name;
+    const finalPath = `${path}/${name}`;
+    button.onclick = () => { loader(finalPath) };
+    container.appendChild(button);
+  };
+  const exerciseDetails = document.getElementById('exercise-details');
+  renderItem(exercises, 'exercises', exerciseDetails)
 }
+
--- linking/open-in-pytut.js
+++
@@ -1,8 +0,0 @@
-function open_in_pytut() {
-  var code = editor.getValue();
-  var encoded = encodeURIComponent(code);
-  var sanitized = encoded.replace(/\(/g, '%28').replace(/\)/g, '%29');
-  var de_tabbed = sanitized.replace(/%09/g, '%20%20');
-  var url = "http://www.pythontutor.com/live.html#code="+sanitized+"&cumulative=false&curInstr=1&heapPrimitives=nevernest&mode=display&origin=opt-live.js&py=js&rawInputLstJSON=%5B%5D&textReferences=false";
-  window.open(url, '_blank');
-};
